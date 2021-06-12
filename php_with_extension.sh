fa854601035743751a84cb350260ef0841ec576f
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 00:51:03 +0800

    Create php_with_extension.sh

---
+++ php_with_extension.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "This bash required root permission."
+    exit 0
+fi
+
+AVAILABLE_VERSION=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
+
+VERSION=""
+
+read -s "PHP Version: " VERSION
+
+while [[ ! " ${AVAILABLE_VERSION[@]} " =~ " ${VERSION} " ]] do
+    echo "Your input version($VERSION) not supported, please enter another one."
+    read -s "PHP Version: " VERSION
+done
+
+add-apt-repository ppa:ondrej/php
+apt-get update
+# support Laravel, Wordpress, Woocommerce, OpenCart, Magento and related program
+apt-get -y install openssl php${VERSION} php${VERSION}-fpm php${VERSION}-bcmath php${VERSION}-common php${VERSION}-curl\
+    php${VERSION}-json php${VERSION}-mysql php${VERSION}-mbstring php${VERSION}-xml php${VERSION}-zip php${VERSION}-gd\
+    php${VERSION}-soap php${VERSION}-ssh2 php${VERSION}-tokenizer php${VERSION}-intl php${VERSION}-xsl php${VERSION}-mcrypt
+    
+tput reset
+echo "Successful install PHP with version: $VERSION"
