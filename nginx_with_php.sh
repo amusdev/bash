ece8045e277ff1aade30a3112ecc35184c405dc1
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 18:16:44 +0800

    Update nginx_with_php.sh

--- nginx_with_php.sh
+++ nginx_with_php.sh
@@ -4,11 +4,6 @@ source <(curl -s https://amusdev.github.io/bash/common.sh)
 source <(curl -s https://amusdev.github.io/bash/php.sh)
 source <(curl -s https://amusdev.github.io/bash/nginx.sh)
 
-# capture_linux_version from common.sh
-LINUX_OS=$(capture_linux_version)
-# capture_centos_major_verison from common.sh
-CENTOS_MAJOR_VERSION=$(capture_centos_major_verison)
-
 # check_env from common.sh
 check_env
 if [ $? -eq 1 ]; then
@@ -21,6 +16,11 @@ elif [ $? -eq 3 ]; then
     echo "This bash only executable on CentOS 5 - 8."
     exit 1
 fi
+
+# capture_linux_version from common.sh
+LINUX_OS=$(capture_linux_version)
+# capture_centos_major_verison from common.sh
+CENTOS_MAJOR_VERSION=$(capture_centos_major_verison)
 
 install_nginx
 install_php $LINUX_OS $CENTOS_MAJOR_VERSION
