05d4d86cef7fa70a50d9115bfb00696d86d2f879
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 20:00:26 +0800

    Create apache_with_php.sh

---
+++ apache_with_php.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source <(curl -s https://amusdev.github.io/bash/common.sh)
+source <(curl -s https://amusdev.github.io/bash/php.sh)
+source <(curl -s https://amusdev.github.io/bash/apache.sh)
+
+# check_env from common.sh
+check_env
+if [ $? -eq 1 ]; then
+    echo "This bash required root permission."
+    exit 1
+elif [ $? -eq 2 ]; then
+    echo "This bash only executable on Ubuntu, Debian, CentOS."
+    exit 1
+elif [ $? -eq 3 ]; then
+    echo "This bash only executable on CentOS 5 - 8."
+    exit 1
+fi
+
+# capture_linux_version from common.sh
+LINUX_OS=$(capture_linux_version)
+# capture_centos_major_verison from common.sh
+CENTOS_MAJOR_VERSION=$(capture_centos_major_verison)
+
+install_apache
+install_php $LINUX_OS $CENTOS_MAJOR_VERSION
+
+tput reset
+print_apache_finish $LINUX_OS
+print_php_finish
