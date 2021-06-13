e883cf6850bb21c18af79b14af48ca8dd482a021
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 09:18:16 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -6,13 +6,22 @@
 # 3 = CentOS
 # 4 = others
 function capture_linux_version(){
-    if lsb_release -d | grep -q "Ubuntu"; then
-        echo 1
-    elif lsb_release -d | grep -q "Debian"; then
-        echo 2
-    elif lsb_release -d | grep -q "CentOS"; then
-        echo 3
+    if hash lsb_release; then
+        if lsb_release -d | grep -q "Ubuntu"; then
+            echo 1
+        elif lsb_release -d | grep -q "Debian"; then
+            echo 2
+        elif lsb_release -d | grep -q "CentOS"; then
+            echo 3
+        else
+            echo 4
+        fi
     else
-        echo 4
+        if 
     fi
+}
+
+# required CentOS > 5
+function capture_centos_major_verison(){
+    echo $(cat /etc/centos-release | tr -dc '0-9.' | cut -d \. -f1)
 }
