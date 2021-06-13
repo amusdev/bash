ae90f603e5a188f6b80bf9b18c88c93da92a4860
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 12:45:40 +0800

    Update nginx_with_php.sh

--- nginx_with_php.sh
+++ nginx_with_php.sh
@@ -65,6 +65,7 @@ then
     add-apt-repository ppa:ondrej/php
     apt update
     eval "apt -y install openssl php${VERSION} $(build_extension_string "php${VERSION}-")"
+    update-alternatives --set php /usr/bin/php${VERSION}
 elif [ LINUX_OS -eq "Debian" ];
 then
     apt -y install lsb-release apt-transport-https ca-certificates
@@ -72,12 +73,14 @@ then
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
     apt update
     eval "apt -y install openssl php${VERSION} $(build_extension_string "php${VERSION}-")"
+    update-alternatives --set php /usr/bin/php${VERSION}
 elif [ LINUX_OS -eq "CentOS" ];
 then
     yum -y install epel-release
     yum -y install https://rpms.remirepo.net/enterprise/remi-release-$CENTOS_MAJOR_VERSION.rpm
     yum makecache
     eval "yum -y install openssl php${VERSION/\./} $(build_extension_string "php${VERSION/\./}-php-")"
+    update-alternatives --set php /usr/bin/php${VERSION}
 fi
 
 tput reset
