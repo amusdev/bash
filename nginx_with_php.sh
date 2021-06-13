3b9c9c8a2ac6f820538348f0189246b7f0cc8707
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-13 12:17:11 +0800

    Update nginx_with_php.sh

--- nginx_with_php.sh
+++ nginx_with_php.sh
@@ -30,14 +30,33 @@ done
 
 if ! hash nginx; then
     echo "Detected nginx not yet installed, will install nginx first."
-    apt -y install nginx
+    if hash apt; then
+        apt -y install nginx
+    elif hash dnf; then
+        dnf -y install nginx
+    elif hash yum; then
+        yum -y install nginx
+    fi
     
     # make sure linux firewall is opened for Nginx
     if hash ufw; then
         ufw allow 'Nginx HTTP'
         ufw allow 'Nginx HTTPS'
+    elif hash firewall-cmd; then
+        firewall-cmd --permanent --zone=public --add-service=https --add-service=http
+        firewall-cmd --reload
     fi
 fi
+
+EXTENDSIONS=("fpm" "bcmath" "common" "curl" "json" "mysql" "mbstring" "xml" "zip" "gd" "soap" "ssh2" "tokenizer" "intl" "xsl" "mcrypt")
+
+function build_extension_string(){
+    concated_string=""
+    foreach ext EXTENDSIONS
+        concated_string+="$1${ext}"
+    end
+    echo concated_string
+}
 
 if [ LINUX_OS -eq "Ubuntu" ]; then
     # install repository
@@ -45,9 +64,7 @@ if [ LINUX_OS -eq "Ubuntu" ]; then
     add-apt-repository ppa:ondrej/php
     apt update
     # support Laravel, Wordpress, Woocommerce, OpenCart, Magento and related program
-    apt -y install openssl php${VERSION} php${VERSION}-fpm php${VERSION}-bcmath php${VERSION}-common php${VERSION}-curl\
-        php${VERSION}-json php${VERSION}-mysql php${VERSION}-mbstring php${VERSION}-xml php${VERSION}-zip php${VERSION}-gd\
-        php${VERSION}-soap php${VERSION}-ssh2 php${VERSION}-tokenizer php${VERSION}-intl php${VERSION}-xsl php${VERSION}-mcrypt
+    eval "apt -y install openssl php${VERSION} $(build_extension_string "php${VERSION}-")"
 elif [ LINUX_OS -eq "Debian" ]; then
     # install repository
     apt -y install lsb-release apt-transport-https ca-certificates
@@ -55,15 +72,14 @@ elif [ LINUX_OS -eq "Debian" ]; then
     echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
     apt update
     # support Laravel, Wordpress, Woocommerce, OpenCart, Magento and related program
-    apt -y install php${VERSION} php${VERSION}-fpm php${VERSION}-bcmath php${VERSION}-common php${VERSION}-curl\
-        php${VERSION}-json php${VERSION}-mysql php${VERSION}-mbstring php${VERSION}-xml php${VERSION}-zip php${VERSION}-gd\
-        php${VERSION}-soap php${VERSION}-ssh2 php${VERSION}-tokenizer php${VERSION}-intl php${VERSION}-xsl php${VERSION}-mcrypt
+    eval "apt -y install openssl php${VERSION} $(build_extension_string "php${VERSION}-")"
 elif [ LINUX_OS -eq "CentOS" ]; then
     # install repository
     yum -y install epel-release
-    rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-$CENTOS_MAJOR_VERSION.rpm
+    yum -y install https://rpms.remirepo.net/enterprise/remi-release-$CENTOS_MAJOR_VERSION.rpm
+    yum makecache
     # support Laravel, Wordpress, Woocommerce, OpenCart, Magento and related program
-    yum --enablerepo=remi-php${VERSION/\./} install php php-fpm
+    eval "yum -y install openssl php${VERSION/\./} $(build_extension_string "php${VERSION/\./}-php-")"
 fi
 
 tput reset
