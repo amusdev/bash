f22aa63fcd5144f517fd99f326809ddfd70fc0fb
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-16 09:48:08 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -74,11 +74,12 @@ function install_mysql(){
                 ehco "Pin-Priority: 1001" | tee -a /etc/apt/preferences.d/mysql
             else
                 echo "mysql-apt-config mysql-apt-config/select-server select mysql-${VERSION}" | debconf-set-selections
+                echo "mysql-community-server mysql-community-server/default_authentication_plugin select caching_sha2_password" | debconf-set-selections
                 curl -sL https://dev.mysql.com/get/mysql-apt-config_0.8.17-1_all.deb -o ./mysql_config.deb
                 DEBIAN_FRONTEND=noninteractive dpkg -i ./mysql_config.deb
                 apt update
                 DEBIAN_FRONTEND=noninteractive apt install -y mysql-server
-                apt -y install libncurses5 libaio1 libmecab2
+                apt -y install libncurses5 libaio1 libmecab2 libmysqlclient21
             fi
             rm -f ./mysql_config.deb
             SHOULD_EXECUTE_SECURE_INSTALLATION=1
