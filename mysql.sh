83778998757bcb56fb587d0d1795ce0c5ff16437
amus-ryan-har <77886018+amus-ryan-har@users.noreply.github.com>
2021-06-16 12:26:14 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -175,6 +175,8 @@ function install_mysql(){
     if [ $? -eq 1 ]; then
         echo "This bash required root permission."
         exit 1
+    elif [ $? -eq 2 ] | [ $? -eq 3 ]; then
+        echo "Not supported OS, Please refer to minimum support OS to learn more."
     fi
 
     # capture_linux_version from common.sh
